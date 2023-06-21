1435a038bdc2b0c13653cdfa74a4b813c8b5795d
Vishal Vilasrao Ghule <91977540+anvi1505@users.noreply.github.com>
2023-06-21 21:53:57 +0530

    New commit

--- core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
+++ core/src/main/java/training/taylor/timetracker/core/TrackerCoreConfig.java
@@ -2,8 +2,8 @@ package training.taylor.timetracker.core;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import training.taylor.timetracker.core.dao.TimeEntry;
+//import org.springframework.context.annotation.Configuration;
+//import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.ArrayList;
 import java.util.List;
